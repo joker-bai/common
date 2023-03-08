a2fb0a5039805f06dde765bd33761e18dd4df009
coolops-cn <baidjay@163.com>
2023-03-08 11:28:43 +0800

    first commit

--- jaeger.go
+++ jaeger.go
@@ -1,14 +1,12 @@
 package common
 
 import (
-	"github.com/opentracing/opentracing-go"
-	"github.com/uber/jaeger-client-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 	"io"
 	"time"
 )
 
-// 创建jaeger链路追踪实例
+// NewTracer 创建jaeger链路追踪实例
 func NewTracer(serviceName string, addr string) (opentracing.Tracer, io.Closer, error) {
 	cfg := &jaegercfg.Configuration{
 		ServiceName: serviceName,
